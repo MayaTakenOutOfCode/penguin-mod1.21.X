4a11022bad4cce39bed5215ae95f5d41a20cb083
MayaTakenOutOfCode <mayawastaken12@gmail.com>
2024-11-10 13:40:41 +0100

    Initial commit

---
+++ src/main/java/com/maya/penguins/PenguinModClient.java
@@ -0,0 +1,10 @@
+package com.maya.penguins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PenguinModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
